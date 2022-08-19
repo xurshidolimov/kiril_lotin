1cc17327421ae28d8c41edcc3c643576103e8f44
SX Computers <xurshidolimov017@gmail.comgit config --global user.name>
2022-08-19 20:22:15 +0500

    update

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -21,8 +21,3 @@ async def bot_echo(message: types.Message):
         db.add_user(id=message.from_user.id, name=name)
     except sqlite3.IntegrityError as err:
         await bot.send_message(chat_id=ADMINS[0], text=err)
-
-    # Adminga xabar beramiz
-    count = db.count_users()[0]
-    msg = f"{message.from_user.full_name} bazaga qo'shildi.\nBazada {count} ta foydalanuvchi bor."
-    await bot.send_message(chat_id=ADMINS[0], text=msg)
