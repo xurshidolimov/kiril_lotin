c2f169896a7a1dc52cc1be60e03cf24adf81f555
SX Computers <xurshidolimov017@gmail.comgit config --global user.name>
2022-08-19 20:29:43 +0500

    update

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -17,10 +17,4 @@ async def bot_start(message: types.Message):
                     name=name)
     except sqlite3.IntegrityError as err:
         await bot.send_message(chat_id=ADMINS[0], text=err)
-
     await message.answer(f"Salom {name}. Lotin-Kirill telegram botiga xush kelibsiz!")
-
-    # Adminga xabar beramiz
-    count = db.count_users()[0]
-    msg = f"{message.from_user.full_name} bazaga qo'shildi.\nBazada {count} ta foydalanuvchi bor."
-    await bot.send_message(chat_id=ADMINS[0], text=msg)
